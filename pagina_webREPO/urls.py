28fa64009e82c08cae6c02c1b427962a77453645
Juanes109-LM <juanesus269@gmail.com>
2026-02-18 19:12:42 -0500

    Agregadas views y urls de Laura

--- pagina_webREPO/urls.py
+++ pagina_webREPO/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    # Autenticación y Perfil
+    path('registro/', views.registro_usuario, name='registro'),
+    path('login/', views.iniciar_sesion, name='login'),
+    path('logout/', views.cerrar_sesion, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+
+    # Gestión de Inventario (Ropa)
+    path('productos/', views.listar_productos, name='listar_productos'),
+    path('productos/nuevo/', views.agregar_producto, name='agregar_producto'),
+    path('productos/eliminar/<str:producto_id>/', views.eliminar_producto, name='eliminar_producto'),
+    path('productos/editar/<str:producto_id>/', views.editar_producto, name='editar_producto'),
 ]
